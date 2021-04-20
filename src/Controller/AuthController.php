d0486acb3970cc4b8c16d66c90c9858f28908b26
Tr1nome <leosouly@gmail.com>
2021-04-21 00:02:32 +0200

    working auth back

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/auth", name="auth_route")
+ */
+
+class AuthController extends AbstractController
+{
+    protected $dispatcher;
+    public function __construct(EventDispatcherInterface $dispatcher)
+    {
+        $this->eventDispatcher = $dispatcher;
+    }
+
+    /**
+     * @Route("/redirectionTo", name="redirectTo")
+     */
+    public function redirection()
+    {
+        /*$user = $this->getUser();
+        if($user->hasRole('ROLE_ADMIN'))
+        {
+            return $this->redirectToRoute('fos_user_profile_show');
+        }*/
+        return $this->redirectToRoute('to_ng');
+    }
+    
+    /**
+     * @Route(
+     *     "/profile",
+     *     name="auth_profile_api"
+     * )
+     */
+    public function profile()
+    {
+        $user = $this->getUser();
+        return $this->json($user);
+        
+    }
+
+}
